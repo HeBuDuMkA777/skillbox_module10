a79c1afacdc5261ebe2c40997381abbb143b9f49
HeBuDuMkA777 <87447478+HeBuDuMkA777@users.noreply.github.com>
2024-11-10 14:16:10 +0300

    выполнил четвертое задание

--- task_4.py
+++ task_4.py
@@ -14,3 +14,17 @@ print('Задача 4. Простые числа')
 # Введите число: 37.
 
 # Количество простых чисел в последовательности: 4.
+
+amount_num = int(input("Введите количество чисел: "))
+prime_num_counter = 0
+
+for _ in range(amount_num):
+    is_prime = True
+    my_num = int(input("Введите число: "))
+    for divider in range(2, int(my_num ** 0.5) + 1):
+        if my_num % divider == 0:
+            is_prime = False
+            break
+    if is_prime:
+        prime_num_counter += 1
+print("Количество простых чисел в последовательности:", prime_num_counter)
